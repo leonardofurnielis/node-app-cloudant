ff4dd0ef01c42dd70914eba38f77389dc99adf5e
Leonardo Furnielis <leonardofurnielis@outlook.com>
2020-12-22 15:00:38 -0300

    update user schema

--- api/schemas/users.js
+++ api/schemas/users.js
@@ -6,6 +6,7 @@ const schema = Joi.object({
   _id: Joi.any(),
   _rev: Joi.any(),
   created_at: Joi.date().timestamp().default(new Date()),
+  updated_at: Joi.date().timestamp().default(new Date()),
   username: Joi.string().alphanum().min(3).max(30).lowercase().required(),
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
