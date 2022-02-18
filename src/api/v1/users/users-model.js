487f6b4176da81404e5b3d2aa8d960fd68d6c1f6
Leonardo Furnielis <leonardofurnielis@outlook.com>
2022-02-18 14:43:20 -0300

    min updates

--- src/api/v1/users/users-model.js
+++ src/api/v1/users/users-model.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const bcrypt = require('bcryptjs');
-const schema = require('./users-schema');
+const Joi = require('joi');
 const db = require('../../../dao/cloudant');
 
 const dbName = 'users';
+
+const schema = Joi.object({
+  _id: Joi.any(),
+  _rev: Joi.any(),
+  createdAt: Joi.date().timestamp().default(new Date()),
+  updatedAt: Joi.date().timestamp().default(new Date()),
+  username: Joi.string().alphanum().min(3).max(30).lowercase().required(),
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(5).required(),
+  active: Joi.boolean().default(true),
+});
 
 const list = () =>
   db.find(dbName, {
