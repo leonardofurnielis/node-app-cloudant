95f600752edb54eebdb4fb5578c163e36817c858
Leonardo Furnielis <leonardofurnielis@outlook.com>
2022-10-07 11:40:00 -0300

    (structure): tests folder

--- __tests__/controllers/health-controller.test.js
+++ __tests__/controllers/health-controller.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const request = require('supertest');
-const app = require('../../../config');
+const app = require('../../config');
 
 describe('GET /api/health', () => {
   test('When called, should return 200', () => {
